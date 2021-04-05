34930e5ad4923b85f1bb930266c4c381cd72ded8
Daniel Bogucki <bogucki19@gmail.com>
2021-04-05 23:37:11 +0100

    Almost all API Done, next step is UI

---
+++ Spotify/Models/RecommendationsResponse.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendationsResponse.swift
+//  Spotify
+//
+//  Created by Daniel on 05/04/2021.
+//
+
+import Foundation
+
+struct RecommendationsResponse: Codable {
+    let tracks : [AudioTrack]
+}
+
+
