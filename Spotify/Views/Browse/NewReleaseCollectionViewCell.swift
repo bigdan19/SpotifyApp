53cff7d08159a479803a583f64615cdf70977aa5
Daniel Bogucki <bogucki19@gmail.com>
2021-04-08 01:32:19 +0100

    added all necessary views for api calls

---
+++ Spotify/Views/Browse/NewReleaseCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NewReleaseCollectionViewCell.swift
+//  Spotify
+//
+//  Created by Daniel on 08/04/2021.
+//
+
+import UIKit
+
+class NewReleaseCollectionViewCell: UICollectionViewCell {
+    static let identifier = "NewReleaseCollectionViewCell"
+}
---
+++ Spotify/Views/Browse/RecommendedTrackCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecommendedTrackCollectionViewCell.swift
+//  Spotify
+//
+//  Created by Daniel on 08/04/2021.
+//
+
+import UIKit
+
+class RecommendedTrackCollectionViewCell: UICollectionViewCell {
+    static let identifier = "RecommendedTrackCollectionViewCell"
+}
