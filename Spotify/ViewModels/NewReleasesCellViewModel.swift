2f399a94f7fbe81f7875c4e94d765b631c71a6ea
Daniel Bogucki <bogucki19@gmail.com>
2021-04-17 23:33:56 +0100

    created new releases UI and layouts also:
    Added pods ( SDimage, AlaMofire)

---
+++ Spotify/ViewModels/NewReleasesCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  NewReleasesCellViewModel.swift
+//  Spotify
+//
+//  Created by Daniel on 17/04/2021.
+//
+
+import Foundation
+
+struct NewReleasesCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let numberOfTracks: Int
+    let artistName: String
+}
