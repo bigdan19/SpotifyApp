da57df28a9edb440fac2d7d70fa4bd1c8bc7d668
Daniel Bogucki <bogucki19@gmail.com>
2021-05-15 01:29:09 +0100

    test comment 15 may

---
+++ Spotify/Views/TitleHeaderCollectionReusableView.swift
@@ -0,0 +1,39 @@
+//
+//  TitleHeaderCollectionReusableView.swift
+//  Spotify
+//
+//  Created by Daniel on 11/05/2021.
+//
+
+import UIKit
+
+class TitleHeaderCollectionReusableView: UICollectionReusableView {
+    static let identifier = "TitleHeaderCollectionReusableView"
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 22, weight: .regular)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addSubview(label)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = CGRect(x: width/20, y: 5, width: width, height: height)
+    }
+    
+    func configure(with title: String) {
+        label.text = title
+    }
+}
